f42ba8a9b912f60f72817fac1e01aa90f42c59ef
Monet <monetpaul23@gmail.com>
2023-12-17 12:47:08 -0500

    done with  project

--- src/Components/Latest.js
+++ src/Components/Latest.js
@@ -5,123 +5,125 @@ export default function Latest (props){
     
     const [cakeData, setCakedata]= useState(props.data) 
      const [filterCakes, setFilterCakes]= useState('') 
+     const [loading, setLoading]= useState(false)
     
-    function stars(rating){
-      if (rating >=4.5 && rating<=4.9 ){
-  
-          return(
-              <div className="cake__stars">
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star-half-stroke"></i>
-              </div>
-              
-          )
-  
+    
+     function stars(rating){
+        if (rating >=4.5 && rating<=4.9 ){
+    
+            return(
+                <div className="cake__stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star-half-stroke"></i>
+                </div>
+                
+            )
+    
+        }
+        else if(rating >=3.9 && rating<=4.4){
+            return(
+                <div className="cake__stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+            )
+        }
+        else if(rating >=3.4 && rating<=3.8){
+            return(
+                <div className="cake__stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star-half-stroke"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+            )
+        }
+        else if(rating >=2.9 && rating<=3.3){
+            return(
+                <div className="cake__stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+            )
+        }
+        else if(rating >=2.4 && rating<=2.8){
+            return(
+                <div className="cake__stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star-half-stroke"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+            )
+        }
+        else if(rating >=1.9 && rating<=2.3){
+            return(
+                <div className="cake__stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+            )
+        }
+        else if(rating >=1.4 && rating<=1.8){
+            return(
+                <div className="cake__stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star-half-stroke"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+            )
+        }
+        else if(rating >=0.9 && rating<=1.3){
+            return(
+                <div className="cake__stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+            )
+        }
+        else if(rating >=0.1 && rating<=0.8){
+            return(
+                <div className="cake__stars">
+                    <i class="fa-solid fa-star-half-stroke"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+            )
+        } else{
+            return(
+                <div className="cake__stars">
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>  
+                </div>
+             
+            )
+        }
+    
       }
-      else if(rating >=3.9 && rating<=4.4){
-          return(
-              <div className="cake__stars">
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-              </div>
-          )
-      }
-      else if(rating >=3.4 && rating<=3.8){
-          return(
-              <div className="cake__stars">
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star-half-stroke"></i>
-                  <i class="fa-regular fa-star"></i>
-              </div>
-          )
-      }
-      else if(rating >=2.9 && rating<=3.3){
-          return(
-              <div className="cake__stars">
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-              </div>
-          )
-      }
-      else if(rating >=2.4 && rating<=2.8){
-          return(
-              <div className="cake__stars">
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star-half-stroke"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-              </div>
-          )
-      }
-      else if(rating >=1.9 && rating<=2.3){
-          return(
-              <div className="cake__stars">
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-              </div>
-          )
-      }
-      else if(rating >=1.4 && rating<=1.8){
-          return(
-              <div className="cake__stars">
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star-half-stroke"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-              </div>
-          )
-      }
-      else if(rating >=0.9 && rating<=1.3){
-          return(
-              <div className="cake__stars">
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-              </div>
-          )
-      }
-      else if(rating >=0.1 && rating<=0.8){
-          return(
-              <div className="cake__stars">
-                  <i class="fa-solid fa-star-half-stroke"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-                  <i class="fa-regular fa-star"></i>
-              </div>
-          )
-      } else{
-          return(
-              <div className="cake__stars">
-              <i class="fa-regular fa-star"></i>
-              <i class="fa-regular fa-star"></i>
-              <i class="fa-regular fa-star"></i>
-              <i class="fa-regular fa-star"></i>
-              <i class="fa-regular fa-star"></i>  
-              </div>
-           
-          )
-      }
-  
-    }
       
   
  
@@ -129,9 +131,28 @@ export default function Latest (props){
  
     useEffect(()=>{
         if( filterCakes ==="LOW_TO_HIGH"){
-            const loHiCakes=cakeData.sort((a,b)=> a.cost- b.cost) 
-              return  setCakedata([...loHiCakes])
+           setTimeout(()=>{
+            setLoading(pre =>!pre)
+           }, 1000)
+            const loHiCakes=cakeData.sort((a,b)=> (a.salePrice||a.cost)- (b.salePrice||b.cost)) 
+             setCakedata([...loHiCakes])
+             
+             
                }
+        else if(filterCakes=== "HIGH_TO_LOW"){
+            setTimeout(()=>{
+                setLoading(pre =>!pre)
+               }, 1000)
+            const hiLoCakes=cakeData.sort((a,b)=> ( b.salePrice||b.cost) - (a.salePrice||a.cost)) 
+            setCakedata([...hiLoCakes])
+        }
+        else if(filterCakes=== "RATING"){
+            setTimeout(()=>{
+                setLoading(pre =>!pre)
+               }, 1000)
+            const rateCakes=cakeData.sort((a,b)=> b.rating- a.rating) 
+          setCakedata([...rateCakes])
+        }
 
 
     },[filterCakes])
@@ -144,6 +165,8 @@ export default function Latest (props){
    
 //
   function handlechange (event){
+    
+    setLoading(pre=> !pre)
     setFilterCakes(event.target.value)
     
 
@@ -161,7 +184,9 @@ const cakes = cakeData.map((cake)=>{
            {cake.title}
             </div>
             <div className="cake__rating">
+            <div className="cake__stars"> 
             
+            </div>
             {stars(cake.rating)}
             <div> 
                 <p>({cake.rating})</p>
@@ -169,8 +194,15 @@ const cakes = cakeData.map((cake)=>{
            
             </div>
             
-           <div className="cake__price"><span className="latest">${cake.cost}</span></div>
+            {cake.salePrice?
+                <div className="cake__price"><span className="cake__price-sale">${cake.cost}</span><span className="latest">${cake.salePrice}</span></div>
+                
+                :
+                <div className="cake__price"><span className="latest">${cake.cost}</span></div>}
 
+
+
+          
            
         </div>
     )
@@ -196,8 +228,9 @@ const cakes = cakeData.map((cake)=>{
       </select>
       </div>
       
-      <div className="cakes-container ">
-          {cakes}
+      <div className="cakes-container">
+      { loading? <i class="fa-solid fa-spinner cakes__loading--spinner"></i> : cakes}
+         
       </div>
       </div>
   </div>
